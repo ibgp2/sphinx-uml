1b37e28666829045e6719f029e16971ec6a548de
Marc-Olivier Buob <marc-olivier.buob@nokia-bell-labs.com>
2025-06-24 14:09:16 +0200

    License BSD-3

--- docs/conf.py
+++ docs/conf.py
@@ -52,7 +52,7 @@ master_doc = "index"
 
 # General information about the project.
 project = "sphinx_uml"
-copyright = "GPL-3"
+copyright = "BSD-3"
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
--- src/sphinx_uml/__init__.py
+++ src/sphinx_uml/__init__.py
@@ -1,7 +1,7 @@
 __author__ = "Marc-Olivier Buob"
 __maintainer__ = "Marc-Olivier Buob"
 __email__ = "marc-olivier.buob@nokia-bell-labs.com"
-__license__ = "GPL-3"
+__license__ = "BSD-3"
 __version__ = "0.1.0"
 
 
